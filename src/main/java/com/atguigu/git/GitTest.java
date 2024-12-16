817827b59eac89ad4b17df011a5c7377860c9c12
xicheng <ddwuchen@163.com>
2024-12-16 13:28:38 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,7 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello1");
+        System.out.println("hello2");
+        System.out.println("hello3");
     }
 }
