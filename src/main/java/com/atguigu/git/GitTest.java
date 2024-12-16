23af93a3e4c181366e43f7bb1c7d626b5b39fbfb
xicheng <ddwuchen@163.com>
2024-12-16 13:21:10 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Author:wuchen
+ * @Description:TODO
+ * @DateTime:2024/12/16 星期一 13:14
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
